6964fd8aa13ae4ff0f08c9fd8d5a26b38773f3e3
matheus desktop <matheuskbraga9@gmail.com>
2026-09-17 18:06:42 -0300

    Criação do banco com a ORM.

--- app_laboratory/models.py
+++ app_laboratory/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Notebooks(models.Model):
+    numero = models.CharField(max_length=50, unique=True,)
+    pessoa_emprestada = models.CharField(max_length=150, blank=True, null=True,)
+    esta_emprestado = models.BooleanField(default=False, verbose_name="Está Emprestado?")
+
+def __str__(self):
+    status = "Emprestado" if self.esta_emprestado else "Disponível"
+    return f"Notebook {self.numero} - {status}"
